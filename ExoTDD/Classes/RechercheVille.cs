86f20f42907503c340f7dfe4bf2992384d56e9c6
llaumegui <guillaume.lfbvre@gmail.com>
2025-02-07 12:32:46 +0100

    feat: add RechercheVille constructor and TestInitialize

--- ExoTDD/Classes/RechercheVille.cs
+++ ExoTDD/Classes/RechercheVille.cs
@@ -2,6 +2,11 @@ namespace ExoTDD.Classes;
 
 public class RechercheVille {
     private List<String> _villes;
+
+    public RechercheVille(List<String> villes)
+    {
+        _villes = villes;
+    }
    
     public List<String> Rechercher(String mot)  {
         throw new NotImplementedException();
--- TestExoTDD/Classes/TestRechercheVille.cs
+++ TestExoTDD/Classes/TestRechercheVille.cs
@@ -1,6 +1,38 @@
+using ExoTDD.Classes;
+using NUnit.Framework;
+
 namespace TestExoTDD.Classes;
 
+[TestFixture]
 public class TestRechercheVille
 {
-    
+    private List<String> _villes;
+    private RechercheVille _rechercheVille;
+
+    [SetUp]
+    public void TestInitialize()
+    {
+        // simulate a db pull
+        _villes = new List<String>()
+        {
+            "Paris",
+            "Budapest",
+            "Skopje",
+            "Rotterdam",
+            "Valence",
+            "Vancouver",
+            "Amsterdam",
+            "Vienne",
+            "Sydney",
+            "New York",
+            "Londres",
+            "Bangkok",
+            "Hong Kong",
+            "Dubaï",
+            "Rome",
+            "Istanbul"
+        };
+        
+        _rechercheVille = new RechercheVille(_villes);
+    }
 }
